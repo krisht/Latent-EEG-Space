9522a2e735de274ca25bf7f196722297b6bb7483
zhengqixi <zhengqixi@github.com>
2017-04-30 19:34:55 -0400

    should work now....

--- session0/tester.py
+++ session0/tester.py
@@ -8,16 +8,22 @@ import numpy as np
 
 
 class BrainNet:
-    def __init__(self, input_shape=[3,  71, 125], num_output=128, num_classes=6, restore_dir=None):
+    def __init__(self, input_shape=[None,  71, 125], num_output=128, num_classes=6, restore_dir=None):
         self.sess = tf.Session()
+        self.num_classes = num_classes
+        self.num_output = num_output
+        self.input_shape = input_shape
         if restore_dir is not None:
             dir = tf.train.Saver()
             dir.restore(self.sess, restore_dir)
         else:
-            self.build_model(input_shape, num_output)
-        self.num_classes = num_classes
+            #only for inference time usage....
+            #i.e you give it a sample and it determines the embedding. This is for use in a k-cluster sort algorithm
+            #Not for training
+            self.inference_input = tf.placeholder(tf.float32, shape=input_shape)
+            self.inference_model = self.get_model(self.inference_input, reuse=False)
 
-    def triplet_loss(self, total, alpha):
+    def triplet_loss(self, alpha):
         """Calculate the triplet loss according to the FaceNet paper
 
         Args:
@@ -28,15 +34,20 @@ class BrainNet:
         Returns:
           the triplet loss according to the FaceNet paper as a float tensor.
         """
-        anchor, positive, negative = total
+        self.anchor = tf.placeholder(tf.float32, shape=self.input_shape)
+        self.positive = tf.placeholder(tf.float32, shape=self.input_shape)
+        self.negative = tf.placeholder(tf.float32, shape=self.input_shape)
+        anchor = self.get_model(self.anchor, reuse=True)
+        positive = self.get_model(self.positive, reuse=True)
+        negative = self.get_model(self.negative, reuse=True)
         with tf.variable_scope('triplet_loss'):
             pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)))
             neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)))
 
             basic_loss = tf.add(tf.subtract(pos_dist, neg_dist), alpha)
             loss = tf.reduce_mean(tf.maximum(basic_loss, 0.0))
+            return loss
 
-        return loss
 
     def get_triplets(self, ax=None, px=None, nx=None, choice=None):
 
@@ -70,56 +81,40 @@ class BrainNet:
         return np.vstack([a,p,n])
 
 
-    def build_model(self, input_shape, num_output):
-        self.inputs = tf.placeholder(tf.float32, shape=input_shape, name='input')
-        self.num_output = num_output
-        self.input_shape = input_shape
-        self.keep_prob = 0.5
-
-
+    def get_model(self, input, reuse=False):
         with slim.arg_scope([slim.layers.conv2d, slim.layers.fully_connected],
                            weights_initializer=tf.contrib.layers.xavier_initializer(seed=random.random(), uniform=True),
-                           weights_regularizer=slim.l2_regularizer(1e-3)):
-            net = tf.expand_dims(self.inputs, axis=3)
-            print(net)
+                           weights_regularizer=slim.l2_regularizer(1e-3), reuse=reuse):
+            net = tf.expand_dims(input, axis=3)
             net = slim.layers.conv2d(net, num_outputs=32, kernel_size=4, scope='conv1', trainable=True)
-            print(net)
             net = slim.layers.max_pool2d(net, kernel_size=3, scope='maxpool1')
-            print(net)
             net = slim.layers.batch_norm(net, trainable=True)
-            print(net)
             net = slim.layers.conv2d(net, num_outputs=64, kernel_size=5, scope='conv2', trainable=True)
-            print(net)
             net = slim.layers.max_pool2d(net, kernel_size=3, scope='maxpool2')
-            print(net)
             net = slim.layers.batch_norm(net, trainable=True)
-            print(net)
             net = slim.layers.flatten(net, scope='flatten');
-            print(net)
             net = slim.layers.fully_connected(net, 256, scope='fc1', trainable=True)
-            print(net)
             net = slim.layers.fully_connected(net, 1024, scope='fc2', trainable=True)
+            net = slim.layers.fully_connected(net, self.num_output, activation_fn=None, weights_regularizer=None, scope='output')
             print(net)
-            net = slim.layers.fully_connected(net, num_output, activation_fn=None, weights_regularizer=None)
-            print(net)
-            self.net = net
+            return net
 
 
     def train_model(self, learning_rate, keep_prob, train_data, batch_size, train_epoch, outdir=None):
-        self.optim = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize()
+        loss = self.triplet_loss(alpha=3)
+        optim= tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss=loss)
         self.sess.run(tf.global_variables_initializer())
 
 
 
         for ii in range(0, batch_size):
             feeder = self.get_triplets()
-            out = self.sess.run(self.net, feed_dict={self.inputs: feeder})
 
-            anchor, positive, negative = out
+            anchor = feeder[0]
+            positive = feeder[1]
+            negative = feeder[2]
 
-            loss = self.triplet_loss(anchor, positive, negative, alpha=3)
-
-            _, loss = self.sess.run([optim, loss])
+            _, loss = self.sess.run([optim, loss], feed_dict={self.anchor:anchor, self.positive:positive, self.negative:negative})
 
             print(loss)
 
