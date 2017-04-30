a5e9a7c157e40d9612024db4fde18253936a2230
Krishna <krisht@cooper.edu>
2017-04-30 16:40:17 -0400

    Added

--- session0/tester.py
+++ session0/tester.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 class BrainNet:
-    def __init__(self, input_shape=[None, 71, 125], num_output=128, num_classes=6, restore_dir=None):
+    def __init__(self, input_shape=[3,  71, 125], num_output=128, num_classes=6, restore_dir=None):
         self.sess = tf.Session()
         if restore_dir is not None:
             dir = tf.train.Saver()
@@ -16,6 +16,59 @@ class BrainNet:
         else:
             self.build_model(input_shape, num_output)
         self.num_classes = num_classes
+
+    def triplet_loss(self, total, alpha):
+        """Calculate the triplet loss according to the FaceNet paper
+
+        Args:
+          anchor: the embeddings for the anchor images.
+          positive: the embeddings for the positive images.
+          negative: the embeddings for the negative images.
+
+        Returns:
+          the triplet loss according to the FaceNet paper as a float tensor.
+        """
+        anchor, positive, negative = total
+        with tf.variable_scope('triplet_loss'):
+            pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)))
+            neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)))
+
+            basic_loss = tf.add(tf.subtract(pos_dist, neg_dist), alpha)
+            loss = tf.reduce_mean(tf.maximum(basic_loss, 0.0))
+
+        return loss
+
+    def get_triplets(self, ax=None, px=None, nx=None, choice=None):
+
+        self.artf0 = np.load('artf0.npz')
+        self.bckg0 = np.load('bckg0.npz')
+        self.eybl0 = np.load('eybl0.npz')
+        self.gped0 = np.load('gped0.npz')
+        self.spsw0 = np.load('spsw0.npz')
+        self.pled0 = np.load('pled0.npz')
+
+        choices = ['bckg', 'eybl']
+
+        ii = random.randint(0, len(self.artf0['arr_0']))
+        jj = random.randint(0, len(self.artf0['arr_0']))
+        choice = random.choice(choices)
+
+        a = self.artf0['arr_0'][ii]
+        a = np.expand_dims(a, 0)
+        p = self.artf0['arr_0'][jj]
+        p = np.expand_dims(p, 0)
+
+        if choice == 'bckg' and len(self.bckg0['arr_0']) != 0:
+            kk = random.randint(0, len(self.bckg0['arr_0']))
+            n = self.bckg0['arr_0'][kk]
+            n = np.expand_dims(n, 0)
+        elif choice == 'eybl' and len(self.eybl0['arr_0']) != 0:
+            kk = random.randint(0, len(self.eybl0['arr_0']))
+            n = self.eybl0['arr_0'][kk]
+            n = np.expand_dims(n, 0)
+
+        return np.vstack([a,p,n])
+
 
     def build_model(self, input_shape, num_output):
         self.inputs = tf.placeholder(tf.float32, shape=input_shape, name='input')
@@ -29,99 +82,49 @@ class BrainNet:
                            weights_regularizer=slim.l2_regularizer(1e-3)):
             net = tf.expand_dims(self.inputs, axis=3)
             print(net)
-            net = slim.layers.conv2d(net, num_outputs=32, kernel_size=5, scope='conv1')
+            net = slim.layers.conv2d(net, num_outputs=32, kernel_size=4, scope='conv1', trainable=True)
             print(net)
-            net = slim.layers.max_pool2d(net, kernel_size=3)
+            net = slim.layers.max_pool2d(net, kernel_size=3, scope='maxpool1')
             print(net)
-            net = slim.layers.batch_norm(net)
+            net = slim.layers.batch_norm(net, trainable=True)
             print(net)
-
-
-            net = slim.layers.conv2d(net, num_outputs = 64, kernel_size=5, scope='conv2')
+            net = slim.layers.conv2d(net, num_outputs=64, kernel_size=5, scope='conv2', trainable=True)
             print(net)
-            net = slim.layers.max_pool2d(net, kernel_size=3)
+            net = slim.layers.max_pool2d(net, kernel_size=3, scope='maxpool2')
             print(net)
-            net = slim.layers.batch_norm(net)
+            net = slim.layers.batch_norm(net, trainable=True)
             print(net)
-            net = slim.layers.flatten(net, scope='flatten')
+            net = slim.layers.flatten(net, scope='flatten');
             print(net)
-            net = slim.layers.fully_connected(net, 256)
+            net = slim.layers.fully_connected(net, 256, scope='fc1', trainable=True)
             print(net)
-            net = slim.layers.fully_connected(net, 1024)
-            print(net)
-            net = slim.layers.dropout(net, keep_prob = self.keep_prob)
+            net = slim.layers.fully_connected(net, 1024, scope='fc2', trainable=True)
             print(net)
             net = slim.layers.fully_connected(net, num_output, activation_fn=None, weights_regularizer=None)
             print(net)
             self.net = net
-            self.sess.run(tf.global_variables_initializer())
 
 
     def train_model(self, learning_rate, keep_prob, train_data, batch_size, train_epoch, outdir=None):
+        self.optim = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize()
+        self.sess.run(tf.global_variables_initializer())
 
 
-
-        
-
-        artf0 = np.load('artf0.npz')
-        print(artf0['arr_0'].shape)
-        bckg0 = np.load('bckg0.npz')
-        print(bckg0['arr_0'].shape)
-        eybl0 = np.load('eybl0.npz')
-        print(eybl0['arr_0'].shape)
-        gped0 = np.load('gped0.npz')
-        print(gped0['arr_0'].shape)
-        spsw0 = np.load('spsw0.npz')
-        print(spsw0['arr_0'].shape)
-        pled0 = np.load('pled0.npz')
-        print(pled0['arr_0'].shape)
 
         for ii in range(0, batch_size):
-            choices = ['bckg', 'eybl']
+            feeder = self.get_triplets()
+            out = self.sess.run(self.net, feed_dict={self.inputs: feeder})
 
-            ii = random.randint(0, len(artf0['arr_0']))
-            jj =  random.randint(0, len(artf0['arr_0']))
-            choice = random.choice(choices)
+            anchor, positive, negative = out
 
-            anchor = artf0['arr_0'][ii]
-            anchor = np.expand_dims(anchor, axis=0)
-            positive = artf0['arr_0'][jj]
-            positive = np.expand_dims(positive, axis=0)
-            negative = [] 
+            loss = self.triplet_loss(anchor, positive, negative, alpha=3)
 
-            if choice=='bckg' and len(bckg0['arr_0']) != 0:
-                kk = random.randint(0, len(bckg0['arr_0']))
-                negative = bckg0['arr_0'][kk]
-            elif choice == 'eybl' and len(eybl0['arr_0']) != 0:
-                kk = random.randint(0, len(eybl0['arr_0']))
-                negative = eybl0['arr_0'][kk]
-            # elif choice =='gped' and len(gped0['arr_0']) != 0:
-            #     kk = random.randint(0, len(gped0['arr_0']))
-            #     negative = gped0['arr_0'][kk]
-            # elif choice=='spsw' and len(spsw0['arr_0']) != 0:
-            #     kk = random.randint(0, len(spsw0['arr_0']))
-            #     negative = spsw0['arr_0'][kk]
-            # elif choice=='pled' and len(pled0['arr_0']) != 0:
-            #     kk = random.randint(0, len(pled0['arr_0']))
-            #     negative = pled0['arr_0'][kk]
+            _, loss = self.sess.run([optim, loss])
 
-            negative = np.expand_dims(negative, axis=0)
-
-            print(negative.shape)
+            print(loss)
 
 
-            positiveOut = self.sess.run(self.net, feed_dict={self.inputs: positive})
-            negativeOut = self.sess.run(self.net, feed_dict = {self.inputs: negative})
-            anchorOut = self.sess.run(self.net, feed_dict={self.inputs: anchor})
 
-            self.positiveLoss = tf.reduce_mean(0.5 * tf.square(positiveOut - anchorOut))
-            self.negativeLoss = - tf.reduce_mean(0.5 * tf.square(negativeOut - anchorOut))
-
-            self.positiveOptim = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.positiveLoss)
-            self.negativeOptim = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.negativeLoss)
-
-            positiveloss, negativeloss, _, _ = self.sess.run([self.positiveLoss, self.negativeLoss, self.positiveOptim, self.negativeOptim])
-            print(ii, positiveloss, negativeloss)
         #
         #
         #
